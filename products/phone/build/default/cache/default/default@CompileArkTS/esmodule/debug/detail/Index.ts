1402d15dfbc8199f7da7732c67b51a73a802c116
isa7129 <1192512450@qq.com>
2026-05-18 20:11:39 +0800

    删除多余文件，只保留homework1和homework2

--- products/phone/build/default/cache/default/default@CompileArkTS/esmodule/debug/detail/Index.ts
+++
@@ -1,4 +0,0 @@
-export { ProductHome } from "@bundle:com.huawei.multishoppingpricecomparison/phone@detail/ets/view/ProductHome";
-export { LiveHome } from "@bundle:com.huawei.multishoppingpricecomparison/phone@detail/ets/view/LiveHome";
-export { ProductMoreDetail } from "@bundle:com.huawei.multishoppingpricecomparison/phone@detail/ets/view/ProductMoreDetail";
-export { DetailConstants } from "@bundle:com.huawei.multishoppingpricecomparison/phone@detail/ets/constants/DetailConstants";
--- products/phone/build/default/cache/default/default@CompileArkTS/esmodule/debug/home/Index.ts
+++
@@ -1,3 +0,0 @@
-export { Home } from "@bundle:com.huawei.multishoppingpricecomparison/phone@home/ets/view/Home";
-export { DetailShoppingBagView } from "@bundle:com.huawei.multishoppingpricecomparison/phone@home/ets/view/ShoppingBagContent";
-export { ClassifyConstants } from "@bundle:com.huawei.multishoppingpricecomparison/phone@home/ets/constants/ClassifyConstants";
